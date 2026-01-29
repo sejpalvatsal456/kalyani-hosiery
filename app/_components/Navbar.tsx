7d0181679854999effc4aa49b4e607cc5975c8b5
Vatsal <sejpalvatsal456@gmail.com>
2026-01-29 21:04:36 +0530

    Update the navbar style

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -12,10 +12,12 @@ import Image from "next/image";
 export default function Navbar({
   activePage,
   navLinksData,
+  setPage,
   cartCount,
 }: {
   activePage: string;
   navLinksData: navLinksDataType[];
+  setPage: (val: string) => void; 
   cartCount: number;
 }) {
   const [open, setOpen] = useState<boolean>(false);
@@ -25,26 +27,20 @@ export default function Navbar({
       <li
         key={key}
         className={
-          "hover:font-medium hover:underline underline-offset-3 " +
-          (activePage === navLink.tag ? "underline" : "")
+          "w-full h-10 text-center hover:font-medium border-[#fc2167] cursor-pointer hover:border-b-3 hover:border-[#fc2167] " +
+          (activePage === navLink.tag ? "border-b-3 text-[#fc2167]" : "")
         }
+        onClick = {() => setPage(navLink.tag)}
       >
-        <a href={"/" + navLink.tag}>{navLink.name}</a>
+        <span>{navLink.name}</span>
       </li>
     );
   });
 
   return (
-    <nav className="shadow-sm px-6 md:px-20">
+    <nav className="border-b-1 border-gray-300">
       {/* Top Bar */}
-      <div className="flex justify-between items-center h-20">
-        {/* Navbar hamburger - mobile */}
-        <button
-          onClick={() => setOpen(!open)}
-          className="md:hidden cursor-pointer"
-        >
-          {!open ? <HiMenu size={25} /> : <HiX size={25} />}
-        </button>
+      <div className="flex justify-between items-center h-20 pl-6 pr-10">
 
         {/* Logo */}
         <a href="/">
@@ -63,11 +59,11 @@ export default function Navbar({
         <form
           action=""
           method="GET"
-          className="hidden md:flex items-center bg-[#f5f5f5] px-5 gap-5 rounded"
+          className="flex items-center bg-[#f5f5f5] px-5 gap-5 rounded"
         >
           <CiSearch size={20} className="h-10 " />
           <input
-            className="h-10 focus:outline-none"
+            className="w-30 md:w-auto h-10 focus:outline-none"
             placeholder="Search..."
             type="text"
             name="serachInput"
@@ -100,13 +96,19 @@ export default function Navbar({
               </div>
             )}
           </li>
+          <li className="flex items-center gap-5">
+            <FaRegUser size={20} />
+          </li>
         </ul>
       </div>
+
+      {/* Mobile links */}
+      <ul className="flex justify-between md:hidden">{navLinksEl}</ul>
 
       {/* Foldable Menu for mobile view */}
       <div
         className={
-          "md:hidden transistion-[max-height] duration-300 ease-in-out " + (open ? "max-h-100 opacity-100" : "max-h-0 opacity-0")
+          "md:hidden px-6 transistion-[max-height] duration-300 ease-in-out " + (open ? "max-h-100 opacity-100" : "max-h-0 opacity-0")
         }
       >
         {/* Mobile search */}
@@ -123,15 +125,9 @@ export default function Navbar({
           />
         </form>
 
-        {/* Mobile links */}
-        <ul className="flex flex-col gap-6 py-6 mb-5">{navLinksEl}</ul>
-
         {/* Mobile icons */}
         <ul className="flex flex-col gap-5 pb-6">
-          <li className="flex items-center gap-5">
-            <FaRegUser size={20} />
-            <span>Profile</span>
-          </li>
+          
         </ul>
       </div>
     </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { navLinksDataType } from "@/lib/typeDefinitions";
 import Header from "./_components/Header";
 import Navbar from "./_components/Navbar";
+import { useState } from "react";
 
 const navLinksData: navLinksDataType[] = [
   { name: "Men", tag: "men" },
@@ -9,10 +12,14 @@ const navLinksData: navLinksDataType[] = [
 ]
 
 export default function Home() {
+
+  const [page, setPage] = useState<string>("men")
+
   return (
     <>
       <Header visibility={false} />
-      <Navbar activePage="home" navLinksData={navLinksData} cartCount={2} />
+      <Navbar activePage={page} setPage={setPage} navLinksData={navLinksData} cartCount={2} />
+      <h1 className="text-2xl m-5 font-semibold">{page}</h1>
     </>
   );
 }
