0727802de4d90c70ee20adf3351574cd8fa1f62c
Vatsal <sejpalvatsal456@gmail.com>
2026-02-04 20:25:25 +0530

    Updated Product Overview Page - mobile

--- app/_components/ProductOverview.tsx
+++ app/_components/ProductOverview.tsx
@@ -12,6 +12,11 @@ const navLinksData: navLinksDataType[] = [
   { name: "Kids", tag: "kids" },
 ];
 
+const getDiscount = (mrp: number, price: number) => {
+  const discount = ((mrp - price) / mrp) * 100;
+  return Math.round(discount);
+};
+
 export default function ProductOverview({
   productData,
 }: {
@@ -23,6 +28,7 @@ export default function ProductOverview({
     colors: string[];
     sizes: string[];
     links: string[];
+    desc: { key: string; value: string }[];
   };
 }) {
   const [search, setSearch] = useState<string>("");
@@ -30,6 +36,15 @@ export default function ProductOverview({
   const [selectedSize, setSelectedSize] = useState<number>(0);
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    const selection = {
+      color: productData.colors[selectedColor],
+      size: productData.sizes[selectedSize],
+    };
+    alert(`Color: ${selection.color} | Size: ${selection.size}`);
+  };
 
   return (
     <>
@@ -41,19 +56,25 @@ export default function ProductOverview({
         setPage={() => {}}
         cartCount={2}
       />
-      <div className="h-[100vh] w-[100vw] mt-10 flex justify-evenly">
+      <div className="h-[100vh] w-[100vw] mt-10 flex flex-col md:flex-row justify-evenly">
         {/* Photo Privews */}
-        <div className="w-[45vw] h-full flex justify-center">
+        <div className="w-[95vw] md:w-[45vw] h-full flex justify-center">
           <img src={productData.links[selectedColor]} alt="" />
         </div>
 
         {/* Product Overview */}
-        <div className="w-[45vw] h-full">
+        <div className="w-full md:w-[45vw] mt-10 ml-5  md:mt-0 md:ml-0 h-full">
           {/* Title and price display */}
 
-          <div className="flex justify-between">
-            <h1 className="text-2xl font-medium">{productData.title}</h1>
-            <h1 className="text-2xl font-medium">Rs. {productData.price}</h1>
+          <h1 className="text-3xl font-bold mb-5">{productData.title}</h1>
+          <span className="line-through text-gray-500">
+            ₹ {productData.mrp}
+          </span>
+          <div className="flex gap-10 items-center">
+            <h1 className="text-2xl font-medium">₹ {productData.price}</h1>
+            <span className="text-sm text-white h-5 bg-green-500 px-3 rounded-full font-semibold">
+              {getDiscount(productData.mrp, productData.price)}% off
+            </span>
           </div>
 
           {/* Product description */}
@@ -94,7 +115,7 @@ export default function ProductOverview({
 
           <div className="flex flex-col gap-4">
             <h1 className="text-lg mt-5 font-semibold">Sizes</h1>
-            <div className="flex gap-10 mx-5">
+            <div className="flex gap-10 mx-5 w-[80vw] md:w-[40vw] grid grid-cols-4 md:grid-cols-5 gap-4">
               {productData.sizes.map((size: string, key) => {
                 return (
                   <button
@@ -117,9 +138,9 @@ export default function ProductOverview({
           {/* Buy Now button */}
 
           <button
-            onClick={() => setIsLoading(true)}
+            onClick={handleClick}
             className={
-              `mt-5 w-140 mx-4 h-14 rounded font-semibold text-white text-lg hover:bg-white hover:text-[#4d3af3] hover:border-[#4d3af3] hover:border-1 transition-all duration-300s ` +
+              `mt-5 w-[80vw] md:w-140 mx-4 h-14 rounded font-semibold text-white text-lg hover:bg-white hover:text-[#4d3af3] hover:border-[#4d3af3] hover:border-1 transition-all duration-300s ` +
               (isLoading
                 ? "bg-[#2f2394] cursor-not-allowed"
                 : "bg-[#4d3af3] cursor-pointer")
@@ -131,10 +152,16 @@ export default function ProductOverview({
 
           <div className="flex flex-col gap-2">
             <h1 className="text-lg mt-5 font-semibold">Descriptions</h1>
-            <h2 className="mx-5 text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequatur, tempora!
-            </h2>
+            <ul className="mb-10 mt-5 flex flex-col gap-3">
+              {productData.desc.map((pair, key) => {
+                return (
+                  <li key={key} className="text-gray-500 font-semibold">
+                    <span className="inline-block w-[40vw]">{pair.key}: </span>
+                    <span>{pair.value}</span>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
         </div>
       </div>
--- app/product/[prodId]/page.tsx
+++ app/product/[prodId]/page.tsx
@@ -7,10 +7,15 @@ const productData = {
   mrp: 250,
   subtitle: "Description...............",
   colors: ["9e846d", "956743"],
-  sizes: [ "XS", "S", "L", "XL" ],
+  sizes: [ "XS", "S", "L", "XL", "XXL" ],
   links: [
     "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/MAY/31/tlOSHE7H_ae3c96e82f9f4515b46b452418cc83f3.jpg",
     "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/JUNE/10/4duT7YRL_1bc6681414a14f1d9f5bfdf2f492c68c.jpg"
+  ],
+  desc: [
+    { key: "Cloth Material", value: "Cotton" },
+    { key: "Cloth Material", value: "Cotton" },
+    { key: "Cloth Material", value: "Cotton" },
   ]
 }
 
