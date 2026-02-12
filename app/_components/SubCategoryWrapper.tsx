abc89064ff4497590bcad039515926f1837cc358
sejpalvatsal456 <sejpalvatsal456@gmail.com>
2026-02-12 21:32:50 +0530

    Updated SubCategory Page - mobile and it's backed

--- app/_components/SubCategoryWrapper.tsx
+++ app/_components/SubCategoryWrapper.tsx
@@ -7,12 +7,7 @@ import {
   navLinksDataType,
 } from "@/lib/typeDefinitions";
 import DisplayCard from "./DisplayCard";
-
-const allBrands = [
-  { name: "Levis", stock: 50 },
-  { name: "SZN", stock: 28 },
-  { name: "Moda Rapido", stock: 20 },
-];
+import { FaFilter, FaSort } from "react-icons/fa";
 
 const navLinksData: navLinksDataType[] = [
   { name: "Men", tag: "men" },
@@ -20,14 +15,20 @@ const navLinksData: navLinksDataType[] = [
   { name: "Kids", tag: "kids" },
 ];
 
-const allSizes = ["3XS", "2XS", "XS", "S", "M", "L", "XL", "2XL", "3XL"];
-
 const sortOptions = [
   { title: "Recommended", tag: "R" },
   { title: "What's new", tag: "W" },
   { title: "Price: High to Low", tag: "PHL" },
   { title: "Price: Low to High", tag: "PLH" },
 ];
+
+const getLowestPrice = (product: DisplayProductType) => {
+  return Math.min(
+    ...product.variety.flatMap(v =>
+      v.sizes.map(s => s.sellingPrice)
+    )
+  );
+}
 
 export default function SubCateogryWrapper({
   categoryName,
@@ -51,7 +52,44 @@ export default function SubCateogryWrapper({
     tag: string;
   }>(sortOptions[0]);
 
+  const [isSortCategoryClicked, setIsSortCategoryClicked] = useState<boolean>(false);
+  const [isFilterClicked, setIsFilterClicked] = useState<boolean>(false);
+
   const [search, setSearch] = useState<string>("");
+
+  // Getting all the brands
+  const allBrands = React.useMemo(() => {
+    if (!products) return [];
+
+    const brandMap = new Map<string, number>();
+
+    products.forEach((product) => {
+      const currentCount = brandMap.get(product.brandName) || 0;
+      brandMap.set(product.brandName, currentCount + 1);
+    });
+
+    return Array.from(brandMap.entries()).map(([name, stock]) => ({
+      name,
+      stock,
+    }));
+  }, [products]);
+
+  // Getting all the sizes
+  const allSizes = React.useMemo(() => {
+    if (!products) return [];
+
+    const sizeSet = new Set<string>();
+
+    products.forEach((product) => {
+      product.variety.forEach((v) => {
+        v.sizes.forEach((s) => {
+          sizeSet.add(s.size);
+        });
+      });
+    });
+
+    return Array.from(sizeSet);
+  }, [products]);
 
   useEffect(() => {
     fetch(`/api/subcategories/${categoryName}/${subcategoryName}`, {
@@ -94,7 +132,24 @@ export default function SubCateogryWrapper({
     // Search filter 
     updated = updated.filter((p) => {
       return p.brandName.toLowerCase().includes(search.toLowerCase()) || p.productName.toLowerCase().includes(search.toLowerCase());
-    })
+    });
+
+    // Sorting
+    if (sortCategory.tag === "PHL") {
+      updated.sort((a, b) => getLowestPrice(b) - getLowestPrice(a));
+    }
+
+    if (sortCategory.tag === "PLH") {
+      updated.sort((a, b) => getLowestPrice(a) - getLowestPrice(b));
+    }
+
+    if (sortCategory.tag === "W") {
+      updated.sort(
+        (a, b) =>
+          new Date(b.createdAt).getTime() -
+          new Date(a.createdAt).getTime()
+      );
+    }
 
     setFilteredProducts(updated);
   }, [brands, size, priceRange, sortCategory, search, products]);
@@ -112,9 +167,17 @@ export default function SubCateogryWrapper({
                 );
           }}
         />{" "}
-        <span className="text-sm">{brand.name}</span>{" "}
+        <span className="text-sm">{brand.name.toUpperCase()}</span>{" "}
         <span className="text-gray-400">({brand.stock})</span>
       </li>
+    );
+  });
+
+  const allSizesSelectionEl = allSizes.map((size, key) => {
+    return (
+      <option key={key} value={size}>
+        {size}
+      </option>
     );
   });
 
@@ -137,7 +200,7 @@ export default function SubCateogryWrapper({
             Home / {categoryName} /{" "}
             <span className="font-semibold">{subcategoryName}</span>
           </div>
-          <div>
+          <div className="hidden md:block">
             Sort By:
             <select
               name="sortInput"
@@ -161,8 +224,9 @@ export default function SubCateogryWrapper({
           </div>
         </div>
         <div className="flex justify-evenly mt-5">
+
           {/* Filter options */}
-          <div className="h-[80vh] w-[20vw]">
+          <div className="h-[80vh] w-[20vw] hidden md:block">
             <div className="border-gray-300 border-1 py-3 pl-6">
               <span className="text-md font-semibold">BRAND</span>
               <ul className="mt-3 text-md ml-3">{allBrandSelectionEl}</ul>
@@ -176,13 +240,7 @@ export default function SubCateogryWrapper({
                 onChange={(e) => setSize(e.target.value)}
               >
                 <option value="">All Sizes</option>
-                {allSizes.map((size, key) => {
-                  return (
-                    <option key={key} value={size}>
-                      {size}
-                    </option>
-                  );
-                })}
+                {allSizesSelectionEl}
               </select>
             </div>
             <div className="border-gray-300 border-1 py-3 pl-6">
@@ -224,13 +282,129 @@ export default function SubCateogryWrapper({
               </ul>
             </div>
           </div>
+
           {/* Products */}
-          <div className="h-[80vh] w-[80vw] grid grid-cols-3 gap-5 px-10 py-10 border-t-1 border-gray-300">
+          <div className="h-full w-full md:w-[80vw] grid grid-cols-1 md:grid-cols-4 place-items-center gap-x-5 px-10 pt-10 pb-20 border-t-1 border-gray-300 overflow-y-scroll">
             {filteredProducts?.map((product, key) => {
-              return <DisplayCard key={product._id} productData={product} />;
+              return (
+                <DisplayCard key={product._id} productData={product} />
+              );
             })}
           </div>
         </div>
+
+        {/* Sort By Element for mobile */}
+        <div
+          className={"md:hidden w-full bg-white bottom-[10vh] transition-all duration-300 ease-in-out border-1 border-gray-300 fixed " + (isSortCategoryClicked ? "opacity-100 translate-y-0" : "opacity-0 translate-y-full pointer-events-none")}
+        >
+          <ul>
+            {sortOptions.map((opt, key) => {
+              return (
+                <li 
+                  key={key}
+                  className={"w-full h-15 flex items-center justify-center text-lg " + (opt === sortCategory ? "bg-gray-200": "") }
+                  onClick={e => {
+                    setSortCategory(opt);
+                    setIsSortCategoryClicked(false);
+                  }}
+                >
+                  {opt.title}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <div
+          className={"md:hidden w-full bg-white bottom-[10vh] transition-all duration-300 ease-in-out border-1 border-gray-300 fixed " + (isFilterClicked ? "opacity-100 translate-y-0" : "opacity-0 translate-y-full pointer-events-none")}
+        >
+          {/* Brand Filters */}
+          <div className="p-5 border-b-1 border-gray-300">
+            <span className="text-lg font-medium">BRAND</span>
+            <ul className="mt-3 text-md ml-3">{allBrandSelectionEl}</ul>
+          </div>
+
+          {/* Size Filter */}
+          <div className="p-5 border-b-1 border-gray-300">
+            <span className="text-lg font-medium">SIZES</span>
+            <select
+              name="sizeInput"
+              className="block w-[50vw] text-sm border-1 py-1.5 my-3 ml-3 rounded px-2"
+              id="sizeInput"
+              onChange={(e) => setSize(e.target.value)}
+            >
+              <option value="">All Sizes</option>
+              {allSizesSelectionEl}
+            </select>
+          </div>
+
+          {/* Price Filter */}
+          <div className="p-5 border-b-1 border-gray-300">
+            <span className="text-lg font-medium">PRICE</span>
+            <ul className="mt-3 text-md ml-3 flex flex-col gap-5">
+              <li className="flex gap-5 items-center">
+                Min:
+                <input
+                  type="number"
+                  value={
+                    isNaN(priceRange.low) || priceRange.low === 0
+                      ? ""
+                      : priceRange.low
+                  }
+                  onChange={(e) =>
+                    setPriceRange({
+                      ...priceRange,
+                      low:
+                        e.target.value === "" ? 0 : parseInt(e.target.value),
+                    })
+                  }
+                  className="w-[50vw] border border-default-medium text-heading text-sm rounded focus:ring-brand focus:border-brand px-3 py-1.5 shadow-xs placeholder:text-body"
+                />
+              </li>
+              <li className="flex gap-5 items-center">
+                Max:
+                <input
+                  type="number"
+                  value={isNaN(priceRange.high) ? "" : priceRange.high}
+                  onChange={(e) =>
+                    setPriceRange({
+                      ...priceRange,
+                      high: parseInt(e.target.value),
+                    })
+                  }
+                  className="w-[50vw] border border-default-medium text-heading text-sm rounded focus:ring-brand focus:border-brand px-3 py-1.5 shadow-xs placeholder:text-body"
+                />
+              </li>
+            </ul>
+          </div>
+
+        </div>
+
+        {/* Filter and Sort options for mobile view */}
+        <div className="flex md:hidden w-full bg-white h-[10vh] absolute fixed bottom-0">
+          <button 
+            className="w-[50vw] border-1 border-gray-300 text-xl flex items-center justify-center gap-3"
+            onClick={e => {
+              e.preventDefault();
+              setIsSortCategoryClicked(!isSortCategoryClicked);
+              setIsFilterClicked(false);
+            }}
+          >
+            Sort By
+            <FaSort />
+          </button>
+          <button 
+            className="w-[50vw] border-1 border-gray-300 text-xl flex items-center justify-center gap-3"
+            onClick={e => {
+              e.preventDefault();
+              setIsFilterClicked(!isFilterClicked);
+              setIsSortCategoryClicked(false);
+            }}>
+            Filter
+            <FaFilter />
+            </button>
+        </div>
+
       </div>
     </>
   );
--- lib/typeDefinitions.ts
+++ lib/typeDefinitions.ts
@@ -3,61 +3,6 @@ export type navLinksDataType = {
   tag: string;
 };
 
-// const productData = {
-//   brandName: "Brand",
-//   productName: "T Shirts",
-//   price: 200,
-//   mrp: 250,
-//   colors: ["9e846d", "956743"],
-//   sizes: [ "XS", "S", "L", "XL", "XXL" ],
-//   links: [
-//     "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/MAY/31/tlOSHE7H_ae3c96e82f9f4515b46b452418cc83f3.jpg",
-//     "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/JUNE/10/4duT7YRL_1bc6681414a14f1d9f5bfdf2f492c68c.jpg"
-//   ],
-//   desc: [
-//     { key: "Cloth Material", value: "Cotton" },
-//     { key: "Cloth Material", value: "Cotton" },
-//     { key: "Cloth Material", value: "Cotton" },
-//   ]
-// }
-
-const productData = {
-  brandName: "Brand",
-  productName: "T Shirts",
-  price: 200,
-  mrp: 250,
-  
-  varitey: [
-    {
-      color: "9e846d",
-      link: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/MAY/31/tlOSHE7H_ae3c96e82f9f4515b46b452418cc83f3.jpg",
-      sizes: [
-        { size: "XS", stock: 10 },
-        { size: "S", stock: 10 },
-        { size: "L", stock: 10 },
-        { size: "XL", stock: 10 },
-        { size: "XXL", stock: 10 },
-      ],
-    },
-    {
-      color: "956743",
-      link: "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/JUNE/10/4duT7YRL_1bc6681414a14f1d9f5bfdf2f492c68c.jpg",
-      sizes: [
-        { size: "XS", stock: 10 },
-        { size: "S", stock: 10 },
-        { size: "L", stock: 10 },
-        { size: "XL", stock: 10 },
-      ],
-    },
-  ],
-  thumbnail:
-    "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/2025/MAY/31/tlOSHE7H_ae3c96e82f9f4515b46b452418cc83f3.jpg",
-  desc: [
-    { key: "Cloth Material", value: "Cotton" },
-    { key: "Cloth Material", value: "Cotton" },
-    { key: "Cloth Material", value: "Cotton" },
-  ],
-};
 
 export type Category = {
   _id: string;
@@ -84,6 +29,8 @@ export type ProductDataType = {
     sizes: { size: string; stock: number; mrp: number; sellingPrice: number; }[];
   }[];
   desc: { key: string; value: string }[];
+  createdAt: string,
+  updatedAt: string
 };
 
 export type DisplayProductType = Omit<ProductDataType, "categoryId"|"subcategoryId"|"desc"> & { categoryId: Category, subcategoryId: SubCategory };
