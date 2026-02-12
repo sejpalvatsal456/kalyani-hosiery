2e32e04f1e73c8f3bc86fb30371a10cb61fbc552
sejpalvatsal456 <sejpalvatsal456@gmail.com>
2026-02-12 09:53:35 +0530

    Added contraints to db connections

--- lib/connectDB.ts
+++ lib/connectDB.ts
@@ -4,6 +4,10 @@ export const connectDB = async() => {
     const MONGODB_URI = process.env.MONGODB_URI as string;
     const MONGODB_NAME = process.env.MONGODB_NAME as string;
 
+    if (!MONGODB_NAME || !MONGODB_URI) {
+        throw Error("Failed to fetch DB configs.")
+    }
+
     await mongoose.connect(MONGODB_URI, { dbName: MONGODB_NAME, bufferCommands: false })
         .then(() => {
             console.log("DB is connected successfully");
